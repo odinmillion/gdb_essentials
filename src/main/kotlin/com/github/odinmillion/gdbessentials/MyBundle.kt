2e6adb55234d97e37bbdcd6d0cb17958b4232746
GitHub Action <action@github.com>
2020-08-08 09:39:20 +0000

    Template cleanup

--- src/main/kotlin/com/github/odinmillion/gdbessentials/MyBundle.kt
+++ src/main/kotlin/com/github/odinmillion/gdbessentials/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.odinmillion.gdbessentials
 
 import com.intellij.AbstractBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/odinmillion/gdbessentials/services/MyProjectService.kt
+++ src/main/kotlin/com/github/odinmillion/gdbessentials/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.odinmillion.gdbessentials.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.odinmillion.gdbessentials.MyBundle
 
 class MyProjectService(project: Project) {
 
